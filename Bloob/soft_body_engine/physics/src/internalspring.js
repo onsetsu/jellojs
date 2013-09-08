4d4ab13f5871a0a3181d9eea4a34a57f61a1fc1a
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-09-08 09:02:14 +0200

    removed outdated sources

--- Bloob/soft_body_engine/physics/src/internalspring.js
+++
@@ -1,7 +0,0 @@
-var InternalSpring = function(pmA, pmB, d, k, damp) {
-	this.pointMassA = pmA || 0;
-	this.pointMassB = pmB || 0;
-	this.springD = d || 0.0;
-	this.springK = k || 0.0;
-	this.damping = damp || 0.0;
-};
